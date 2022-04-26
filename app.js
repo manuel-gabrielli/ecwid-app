2ea0606b988b718eba848a4688c795190bd0ec74
manuel-gabrielli <62704299+manuel-gabrielli@users.noreply.github.com>
2022-04-26 16:51:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,22 +12,23 @@
 */
 
 
+// Initialize extra fields
 ec = ec || {};
-      ec.order = ec.order || {};
-      ec.order.extraFields = ec.order.extraFields || {};
-      // Set order surcharge
-      ec.order.extraFields.surcharge = {
-        value: 'Custom charge',
-        options: [
-          {
-            title: 'Custom charge',
-            surcharge: 5,
-          },
-        ],
-        surchargeShortName: {
-          name: 'Surcharge',
-          showSurchargePercentValue: false,
-        },
-        surchargeType: 'absolute',
-      };
-      Ecwid.refreshConfig && Ecwid.refreshConfig();
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
+// Set order surcharge
+ec.order.extraFields.surcharge = {
+    'value': 'Custom charge',
+    "options": [
+    { 
+        "title": "Custom charge",
+        "surcharge": 5
+    },
+    ],
+    "surchargeShortName": {
+        "name": "Surcharge",
+         "showSurchargePercentValue": false
+    },
+    'surchargeType': 'PERCENT'
+}
+Ecwid.refreshConfig && Ecwid.refreshConfig();
