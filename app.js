e04d1b4e18c85ca8db9ed267736a3ac846cc030f
manuel-gabrielli <62704299+manuel-gabrielli@users.noreply.github.com>
2022-04-26 17:37:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ Ecwid.OnPageLoaded.add(function (page) {
         '#ecwid-products > div.ec-loader-wrapper.ecwid-lte-768px.ec-size.ec-size--xxs.ec-size--xs.ec-size--s.ecwid-no-touch.ecwid-supports-cssanimations.ecwid-responsive > div > div > div:nth-child(3) > div > div.ec-store.ec-store__checkout-page > div > div > div.ec-cart__body > div > div.ec-cart__steps > div.ec-cart__step.ec-cart-step.ec-cart-step--payment.ec-cart-step--current > div.ec-cart-step__wrap > div.ec-cart-step__body > div:nth-child(1) > div.ec-cart-step__section > div > div > div > fieldset > div > label.ec-radiogroup__item.ec-radiogroup__item--checked.ec-radiogroup__item--46797-1594116265426.ec-radiogroup__item--Contrassegno > input'
       ).checked == true
     ) {
-      // Initialize extra fields
+      console.log('Hello World!');
+    }
+  }
+});
+
+/*// Initialize extra fields
       ec = ec || {};
       ec.order = ec.order || {};
       ec.order.extraFields = ec.order.extraFields || {};
@@ -25,6 +30,4 @@ Ecwid.OnPageLoaded.add(function (page) {
         surchargeType: 'PERCENT',
       };
       Ecwid.refreshConfig && Ecwid.refreshConfig();
-    }
-  }
-});
+      */
